6d5e81bdef3913ba902c4f91f3a36a894ea8eeb4
Hazem691 <hazemsaber142@gmail.com>
2024-05-27 19:47:09 +0200

    Editing

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(express.json());
 app.use(userRouter) ;
 app.use(postRouter) ;
 app.use(commentRouter);
+app.use("*",(req,res)=>{
+    res.status(404).json({message : "page is not found"})
+})
 app.listen(port,()=>{
     console.log("Server is running now ...");
 })
